3f240c05e7d2c560f822111a4800b29524460bee
John Wright <mrjjwright@gmail.com>
2009-12-19 07:59:19 -0700

    print rails root

--- config/boot.rb
+++ config/boot.rb
@@ -2,6 +2,7 @@
 # Configure your app in config/environment.rb and config/environments/*.rb
 
 RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
+p "RAILS_ROOT is #{RAILS_ROOT}"
 
 module Rails
   class << self
