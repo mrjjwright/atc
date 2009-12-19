005f5032ca8886028288d1639bd51c07f540f82e
John Wright <mrjjwright@gmail.com>
2009-12-19 07:48:25 -0700

    start fb import 20s after restarting server

--- config/initializers/preinitializer.rb
+++ config/initializers/preinitializer.rb
@@ -5,7 +5,7 @@ scheduler = Rufus::Scheduler.start_new
 
 # import feeds for all the old users
 last_updated=Time.zone.now.utc
-scheduler.every "20m" do
+scheduler.every "20m", :first_in => '20s' do 
   #this if block avoids problems with putting my laptop to sleep
   #and then the scheduler replaying a bunch of old scheduled tasks
   if (Time.zone.now.utc.to_i - last_updated.to_i) > 20 #seconds
